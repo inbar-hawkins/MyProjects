eca6b3f2dc15826959bf8f86093bd4739d03f422
Inbar Hawkins <69861957+inbar-hawkins@users.noreply.github.com>
2021-04-09 11:41:55 +0300

    Index, Show, New , Create

--- Udemy/YelpProject/app.js
+++ Udemy/YelpProject/app.js
@@ -1,39 +1,52 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const Campground=require("./models/campground");
+const Campground = require("./models/campground");
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp",{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useUnifiedTopology:true
-     });
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
-db.on("error",console.error.bind(console,"connection error:"));
-db.once("open",()=>{
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
     console.log("Database connected");
 });
 
 const app = express();
 
-app.set("views",path.join(__dirname,"views"));
-app.set("view engine","ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get("/",(req,res)=>{
-    res.render("home");  
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("home");
 })
 
-app.get("/makecampground", async (req,res)=>{
-    const camp = new Campground({title:"My Backyard", description:"cheap camping!"});
-    await camp.save();
-    res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+})
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+})
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+
+})
+
+app.get("/campgrounds/:id", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", { campground });
 })
 
 
-
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("serving on port 3000");
 })
- 
