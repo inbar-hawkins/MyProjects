fbe0ef5135d2be9487273cee03d4fae1b0162d08
Inbar Hawkins <69861957+inbar-hawkins@users.noreply.github.com>
2021-06-03 13:13:16 +0300

    adding session

--- Udemy/YelpProject/app.js
+++ Udemy/YelpProject/app.js
@@ -3,14 +3,28 @@ const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate"); //for layouts
+const ExpressError = require("./utils/ExpressError");//for ErrorsHandler 
+const session = require("express-session");//for saving info and the server and send key cookie back to server
+
 
 const campgrounds = require("./routes/campgrounds"); //for campgrounds routes
 const reviews = require("./routes/reviews"); //for reviews routes
+const sessionConfig = {
+    secret: "MyVeryTopSecert",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    }
+}
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -27,9 +41,11 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/campgrounds", campgrounds);//update campgrounds routes
 app.use("/campgrounds/:id/reviews", reviews);//update reviews routes
+app.use(session(sessionConfig));//define session options
 
 app.get("/", (req, res) => {
     res.render("home");
